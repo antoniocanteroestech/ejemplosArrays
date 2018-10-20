fd47ae838cc9a50b0ec13a4838aa4d05f5836fc4
Antonio Cantero Ruiz <acanteroruiz.dev@gmail.com>
2018-10-20 10:49:44 +0200

    añadidos ejemplos arrays bidimensionales

--- src/es/estech/Main.java
+++ src/es/estech/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /*
+            ARRAYS UNIDIMENSIONALES
+         */
         int[] arrayDeclarado; // declaración de un array
 
         int[] arrayVacio = new int[3]; // inicialización de un array vacío, hay que indicar su tamaño
@@ -25,16 +28,47 @@ public class Main {
         podemos recorrer un array con cualquier tipo de bucle
         por ejemplo un bucle for
          */
-        for (int i = 0; i <= numerosArrayInt.length-1; i++){
-            System.out.print(i + " ");
+        for (int i = 0; i < numerosArrayInt.length; i++){
+            System.out.print(numerosArrayInt[i] + " ");
         }
-
+        System.out.println();
 
         /*
         bucle for in (también llamado foreach o bucle for "mejorado"
          */
         for (int n: numerosArrayInt) {
             System.out.print(n + " ");
+        }
+        System.out.println();
+        System.out.println();
+
+        /*
+            ARRAYS BIDIMENSIONALES
+         */
+        int [][] arrayBidimensional = new int [3][3];
+        arrayBidimensional [0][0] = 1;
+        arrayBidimensional [0][1] = 2;
+        arrayBidimensional [0][2] = 3;
+        arrayBidimensional [1][0] = 4;
+        arrayBidimensional [1][1] = 5;
+        arrayBidimensional [1][2] = 6;
+        arrayBidimensional [2][0] = 7;
+        arrayBidimensional [2][1] = 8;
+        arrayBidimensional [2][2] = 9;
+
+        System.out.println(Arrays.toString(arrayBidimensional)); //aquí no nos vale este método
+        System.out.println();
+
+        /*
+        para imprimir un array bidimensional (o matriz), necesitamos dos estructuras for anidadas
+         */
+        for (int i=0; i < arrayBidimensional.length; i++) { //recorremos las filas
+            System.out.print("|");                          // por el comienzo de cada fila le ponemos un decorador
+            for (int j=0; j < arrayBidimensional[i].length; j++) { // por cada fila recorremos su array
+                System.out.print (arrayBidimensional[i][j]);
+                if (j!=arrayBidimensional[i].length-1) System.out.print("\t"); // tabula si no ha llegado al último elemento
+            }
+            System.out.println("|");                    // al final de cada fila le ponemos un decorador
         }
 
 
